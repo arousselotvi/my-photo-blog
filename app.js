a924bd7c0def170fee36d7c8679a44979a11f30b
Antoine Rousselot <antoine.rousselotvigier@centrale-marseille.fr>
2019-01-20 19:38:27 +0100

    Image upload ok

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
-const cors = require('cors')
+const multer = require('multer')
 const request = require('request')
 
 const app = express();
 const port = 3000;
 
 // Initialization of app variable 
-// Middleware 
-app.use(cors())
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 // Specify where to look for static files
 app.use(express.static(path.join(__dirname, 'public')));
+// Accept headers
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Routes
 app.get('/', (req,res) => {
@@ -30,9 +34,25 @@ app.get('/upload', (req,res) => {
     res.send("Upload form");
 })
 
-app.post('/uploadphoto', (req,res) => {
-    
-})
+// Managing the image upload using multer 
+app.post("/upload-photo", multer({dest: "./public/"}).single('image'), function(req, res) {
+    tempPath=req.file.path;
+    targetPath="./public/" + req.file.originalname;
+    if (path.extname(targetPath).toLowerCase() === ".jpg" || path.extname(targetPath).toLowerCase() === ".jpeg" || path.extname(targetPath).toLowerCase() === ".png")   {
+        fs.rename(tempPath, targetPath, err => {
+            if (err){
+                console.log(err)  
+            } 
+            res.send({"message":"Image sent successfully!"})
+        });
+    } else {
+        fs.unlink(tempPath, err => {
+            if (err) return handleError(err, res);
+            res.send({"message":"Sorry, only .jpg and .png files are accepted"})
+        });
+    }   
+});
+
 // Listen to port 3000
 app.listen(port, () => {
     console.log(`Demarrage du serveur sur le port ${port}`);
