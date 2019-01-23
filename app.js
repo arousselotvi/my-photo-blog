2bfe7e14597cebef2c8b38338591228fadc1a043
Antoine Rousselot <antoine.rousselotvigier@centrale-marseille.fr>
2019-01-23 19:15:45 +0100

    server side is clean

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const multer = require('multer')
 const request = require('request')
-
+const functions = require('./functions')
 const app = express();
 const port = 3000;
 
@@ -19,54 +19,9 @@ app.use(function(req, res, next) {
 });
 
 // Routes
-app.get('/', (req,res) => {
-    res.send("Display photos sorted by upload date");
-})
-
-app.get('/photo/:photoId', (req,res) => {
-    var photoId = req.params.photoId;
-    res.send("Display photo with id " + photoId);
-})
-
-app.get('/upload', (req,res) => {
-    res.send("Upload form");
-})
-
 // Managing the image upload using multer 
 app.post("/upload-photo", multer({dest: "./public/photos"}).single('image'), function(req, res) {
-    // Store the photo
-    let myRegister = JSON.parse(fs.readFileSync('./photo-register.json'));
-    let tempPath=req.file.path;
-    let targetPath = "./public/photos/photo" + myRegister.myPhotos.length + path.extname(req.file.originalname); 
-    console.log(targetPath)
-    if (path.extname(targetPath).toLowerCase() === ".jpg" || path.extname(targetPath).toLowerCase() === ".jpeg" || path.extname(targetPath).toLowerCase() === ".png")   {
-        fs.rename(tempPath, targetPath , err => {
-            if (err){
-                console.log(err)  
-            } else {
-                // Add photo to register
-                let myDate= new Date(0);
-                myDate.setUTCMilliseconds(req.body.date)
-                let myPhotoInfos = {
-                    originalName: req.file.originalname,
-                    actualName: "photo" + myRegister.myPhotos.length,
-                    photoUrl: "http://localhost:3000"+targetPath.slice(1),
-                    uploadEpoch: req.body.date,
-                    uploadDate: myDate,
-                    uploadLocation: ""
-                }
-                // Add the the photo at the beggining of the array so that the photos are organized from newest to oldest
-                myRegister.myPhotos.unshift(myPhotoInfos);
-                fs.writeFileSync('./photo-register.json', JSON.stringify(myRegister))
-                res.send({"message":"Image sent successfully!"})
-            }
-        });
-    } else {
-        fs.unlink(tempPath, err => {
-            if (err) return handleError(err, res);
-            res.send({"message":"Sorry, only .jpg and .png files are accepted"})
-        });
-    }
+    functions.storePhoto(req, res)
 });
 
 app.get('/api/photos', (req,res) => {
